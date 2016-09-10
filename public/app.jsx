a7a95749eb6e0a26632e07948b376885d5812c00
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-10 18:14:58 -0400

    First sate

--- public/app.jsx
+++ public/app.jsx
@@ -5,19 +5,31 @@ var Greeter = React.createClass({
       message: 'This is the default message!'
     };
   },
+  getInitialState: function(){
+    return{
+      name: this.props.name
+    };
+  },
   onButtonClick: function(e){
     e.preventDefault(); //Prevent full page reload
-     var name = this.refs.name.value;
-     alert(name);
+    var nameRef = this.refs.name;
+     var name = nameRef.value;
+     nameRef.value = '';
+
+     if(typeof name === 'string' && name.length > 0){
+       this.setState({
+         name: name
+       });
+     }
   },
   render: function(){
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
 
     return (
       <div>
         <h1> Hello {name}!</h1>
-        <p>{message}</p>
+        <p>{message + '!!'}</p>
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button> Set Name </button>
