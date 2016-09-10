1ce790888cd0932428f4cf39d93ec6899817fdda
Carol Miranda <CarolMiranda@Carols-MacBook-Air.local>
2016-09-10 17:54:23 -0400

    First Form and event handler

--- public/app.jsx
+++ public/app.jsx
@@ -5,13 +5,23 @@ var Greeter = React.createClass({
       message: 'This is the default message!'
     };
   },
+  onButtonClick: function(e){
+    e.preventDefault(); //Prevent full page reload
+     var name = this.refs.name.value;
+     alert(name);
+  },
   render: function(){
     var name = this.props.name;
     var message = this.props.message;
+
     return (
       <div>
         <h1> Hello {name}!</h1>
         <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button> Set Name </button>
+        </form>
       </div>
     );
   }
